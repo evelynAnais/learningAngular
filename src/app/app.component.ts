f8b6d798e638a95ad2a4f2b7015abcbeea97d325
Evelyn Ploughman <evelyn.ploughman@blndspt.com>
2021-11-18 13:43:52 -0700

    adding home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-root',
   template: `
-  <div><h1>{{pageTitle}}</h1>
+  <div><h1>heyyyyy</h1>
     <div>My First Angular App</div>
+    <pm-home></pm-home>
   </div>  
   `
 })
